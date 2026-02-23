915bd9248792f3b4b60459f035ae9a70a4a232b4
Lisandro H G Mosconi Fariña <lisandro.mosconi@gmail.com>
2026-02-23 20:08:00 +0000

    change: changed from with array and finde

--- src/app.js
+++ src/app.js
@@ -4,6 +4,8 @@ let noun = ['falcom', 'cheetah', 'hornet', 'turtles'];
 
 let domainContainer = document.getElementById("domain-list");
 
+const extensions = ["com", "net", "es", "ar", "uy", "cu"];
+
 for (let i = 0; i < pronoun.length; i++) {
   for (let j = 0; j < adj.length; j++) {
     for (let k = 0; k < noun.length; k++) {
@@ -11,27 +13,14 @@ for (let i = 0; i < pronoun.length; i++) {
       let word = noun[k];
       let domain = "";
 
-      if (word.endsWith("com")) {
-        domain = `${pronoun[i]}${adj[j]}${word.slice(0, -3)}.com`;
+      let matchedExtension = extensions.find(ext => word.endsWith(ext));
 
-      } else if (word.endsWith("net")) {
-        domain = `${pronoun[i]}${adj[j]}${word.slice(0, -3)}.net`;
-
-      } else if (word.endsWith("es")) {
-        domain = `${pronoun[i]}${adj[j]}${word.slice(0, -2)}.es`;
-
-      } else if (word.endsWith("ar")) {
-        domain = `${pronoun[i]}${adj[j]}${word.slice(0, -2)}.ar`;
-
-      } else if (word.endsWith("uy")) {
-        domain = `${pronoun[i]}${adj[j]}${word.slice(0, -2)}.uy`;
-
-      } else if (word.endsWith("cu")) {
-        domain = `${pronoun[i]}${adj[j]}${word.slice(0, -2)}.cu`;
-
+      if (matchedExtension) {
+        domain = `${pronoun[i]}${adj[j]}${word.slice(0, -matchedExtension.length)}.${matchedExtension}`;
       } else {
         domain = `${pronoun[i]}${adj[j]}${word}.com`;
       }
+      
       // al final no encontré animales q terminen con uy o cu xD
       domainContainer.innerHTML += `
             <div class="col-md-4">
@@ -49,4 +38,5 @@ for (let i = 0; i < pronoun.length; i++) {
 }
 
 // iba a añadir un botón pero se me fue al carajo todo xD
-// tampoco se me ocurría cómo hacer un loop para los .com .net .es .ar .uy .cu
+// encontré el find, pero no termino de entenderlo al 100%, lo he modificado de algo q encontré por internet y 
+// me funciona, pero no estoy del todo seguro
